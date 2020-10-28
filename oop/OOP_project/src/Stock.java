0abdc29441be41f82ddd457a41f25b5fcc00ecf0
Aman Yadav <amansy04@gmail.com>
2020-10-28 15:55:38 +0530

    Created Stock class

---
+++ oop/OOP_project/src/Stock.java
@@ -0,0 +1,40 @@
+package src;
+
+public class Stock{
+    private String scrip;
+    private String sector;
+    private double openPrice;
+    private double highPrice;
+    private double lowPrice;
+    private double closePrice;
+
+    // Constructor
+    public Stock(String scrip, String sector, double openPrice, double highPrice, double lowPrice, double closePrice) {
+        this.scrip = scrip;
+        this.sector = sector;
+        this.openPrice = openPrice;
+        this.highPrice = highPrice;
+        this.lowPrice = lowPrice;
+        this.closePrice = closePrice;
+    }
+
+    // Getters
+    public String getScrip() {
+        return scrip;
+    }
+    public String getSector() {
+        return sector;
+    }
+    public double getOpenPrice() {
+        return openPrice;
+    }
+    public double getHighPrice() {
+        return highPrice;
+    }
+    public double getLowPrice() {
+        return lowPrice;
+    }
+    public double getClosePrice() {
+        return closePrice;
+    }
+}
